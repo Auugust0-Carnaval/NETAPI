7cdac6d51281f2b57721f74b4d5aa7c897eb56c3
Augusto <augustooliveixa@gmail.com>
2023-05-01 03:16:59 -0300

    Criando classe de modelo e adicionando no contexto de dados {Dbcontext}

--- Program.cs
+++ Program.cs
@@ -62,3 +62,16 @@ app.MapDelete("/todoitems{id}", async (int id, TodoDb db) =>{
 });
 
 app.Run();
+
+class Todo{
+    public int Id {get; set;}
+    public string? Name {get;set;}
+    public bool IsComplete {get; set;}
+}
+
+class TodoDb : DbContext {
+    public TodoDb(DbContextOptions<TodoDb> options)
+        :base(options) { }
+    
+    public DbSet<Todo> Todos => Set<Todo>();
+}
