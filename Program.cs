110e6da5a1b98c0603d69bcddb79cc4086cabee8
Augusto <augustooliveixa@gmail.com>
2023-05-01 03:07:43 -0300

    Adicionando dependencia EntityFramework e setando classe na base de dados

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
 
-app.MapGet("/runtimes", () => "Hello word");
+// app.MapGet("/runtimes", () => "Hello word");
 
-app.Run();
+// app.Run();
 
 
 // OBS: LaunchBrowser tanto no argumento do http e https difine o endpoint do host
+
+
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
 
