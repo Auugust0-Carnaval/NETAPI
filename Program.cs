8889d23a628e774de84c2d302e44527150fe6865
Augusto <augustooliveixa@gmail.com>
2023-05-01 03:13:24 -0300

    Metodo de busca por Id

--- Program.cs
+++ Program.cs
@@ -20,6 +20,12 @@ app.MapGet("/", () => Results.Ok("Projetinho basico em .NET"));
 
 app.MapGet("/todoitems", async (TodoDb db) =>
    await db.Todos.ToListAsync());
-   
+
 app.MapGet("/todoitems/complete", async (TodoDb db) =>
     await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+await db.Todos.FindAsync(id)
+is Todo todo
+    ? Results.Ok(todo)
+    : Results.NotFound("Não encontrado"));
